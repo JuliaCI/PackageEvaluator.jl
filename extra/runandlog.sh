60d8c485e5d91cab3d08d6489be379eef490e9aa
Iain Dunning <iaindunning@gmail.com>
2014-02-26 11:24:30 -0500

    Fixed issue with paths

--- extra/runandlog.sh
+++ extra/runandlog.sh
@@ -1,13 +1,20 @@
 cd /home/idunning/PackageEvaluator.jl/extra
+
 # Set special .julia folder just for this run
 export JULIA_PKGDIR="/home/idunning/pkgtest/.julia"
+
+# Make sure we know where Julia is (for cron job)
+export PATH="$PATH:/home/idunning/julia"
+
 # Make sure its totally empty
 rm -rf $JULIA_PKGDIR
+
 # Initialize and install PackageEvaluator
 /home/idunning/julia/julia -e 'Pkg.init(); Pkg.clone("https://github.com/IainNZ/PackageEvaluator.jl.git")'
+
 # Run the tester on all packages (-1) and export JSON (J)
 # Log STDOUT and STDERR to a file, e.g. genresults_2014-02-25-22.log
-/home/idunning/julia/julia genresults.jl -1 J 2>&1 | tee 
-genresults_$(date '+%Y-%m-%d-%H').log
+/home/idunning/julia/julia genresults.jl -1 J 2>&1 | tee genresults_$(date '+%Y-%m-%d-%H').log
+
 # Post .jsons to status.julialang.org
 /home/idunning/julia/julia postresults.jl
