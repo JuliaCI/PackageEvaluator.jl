c4400b6de5a6b9575820ec590e23c795eaa26c09
Iain Dunning <iaindunning@gmail.com>
2014-03-03 23:25:28 -0500

    Submit test logs, for 0.2 only right now

--- extra/postresults.jl
+++ extra/postresults.jl
@@ -1,15 +1,43 @@
 Pkg.add("Requests")
 Pkg.add("JSON")
 using Requests
+
 json_head = Dict{String, String}()
 json_head["Content-Type"] = "application/json"
 
-for file in readdir()
+# Read in last log file
+allfiles = readdir()
+logfiles = Any[]
+for file in allfiles
+    if contains(file, "genresults_")
+        push!(logfiles,file)
+    end
+end
+sort!(logfiles)
+log_data = split(readall(logfiles[end]),"\n")
+pkg_log = Dict()
+cur_pkg_name = ""
+for line in log_data
+    if contains(line, "##### Current package")
+        cur_pkg_name = strip(split(line, ":")[2])
+        pkg_log[cur_pkg_name] = ""
+    else
+        pkg_log[cur_pkg_name] *= line * "\n"
+    end
+end
+
+for file in allfiles
     if ismatch(r"json", file)
         json_str = readall(file)
-        response = post(URI("http://status.julialang.org/put/package"), json_str, json_head)
+	json_dict = JSON.parse(json_str)
+	if json_dict["name"] in keys(pkg_log)
+            json_dict["testlog"] = pkg_log[json_dict["name"]]
+        else
+            json_dict["testlog"] = "No log! Please file issue."
+        end 
+        response = post(URI("http://status.julialang.org/put/package"), JSON.json(json_dict), 
+json_head)
         println(response)
-        println(response.data)
     end
 end
 
