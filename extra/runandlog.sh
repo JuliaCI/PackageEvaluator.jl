bf27e4254ba59097e6d3b7ecf418c4ca4ac0d42c
Iain Dunning <iaindunning@gmail.com>
2014-05-16 11:57:54 -0400

    Update runandlog.sh

--- extra/runandlog.sh
+++ extra/runandlog.sh
@@ -38,10 +38,10 @@ export LASTGOODCOMMIT="$(python2 $PKGEVALEXTRA/get_last_good_commit.py)"
 git checkout $LASTGOODCOMMIT
 echo "USE_SYSTEM_PCRE = 1" > Make.user
 make
-if [[ ! -f "julia" ]]; then 
+if [[ ! -f "./julia" ]]; then 
   make distcleanall
 fi
-if [[ ! -f "julia" ]]; then 
+if [[ ! -f "./julia" ]]; then 
   make distcleanall
 fi
 export PATH="$ORIGPATH:/home/idunning/pkgtest/julia"
