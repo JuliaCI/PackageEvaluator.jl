a3494699d0f6590e45472c6a642c754559b133eb
Iain Dunning <iaindunning@gmail.com>
2015-06-17 01:56:12 -0400

    More comments and DRY for setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,17 +2,17 @@
 #######################################################################
 # PackageEvaluator
 # https://github.com/IainNZ/PackageEvaluator.jl
-# (c) Iain Dunning 2015
+# (c) Iain Dunning and contributors 2015
 # Licensed under the MIT License
 #######################################################################
 # This script file is the provisioning script run by Vagrant after
 # the VM is created. It sets up the environment for running PkgEval,
 # then runs it to produce the JSON result files.
 #
-# Commandline arguments
-# 1: The Julia version: release | nightly
-# 2: The test set to run: release | releaseAL | releaseMZ |
-#                         nightly | nightlyAL | nightlyMZ
+# Commandline arguments for this script, passed through by Vagrant.
+# 1st: Julia version: release | nightly
+# 2nd: Test set to run: release | releaseAL | releaseMZ |
+#                       nightly | nightlyAL | nightlyMZ
 #######################################################################
 
 
@@ -21,7 +21,7 @@
 # This is an attempt to deal with BinDeps packages trying to install
 # dependencies with apt-get and asking for permissions. Normally we
 # could pass a --yes argument to avoid this, but there isn't a way to
-# do that with BinDpes. We can actually do a global override, which
+# do that with BinDeps. We can actually do a global override, which
 # is what we do here. Since we are piping stuff into a file, the
 # easiest way to sudo the whole thing is to do it in this style.
 sudo su -c 'echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/pkgevalforceyes'
@@ -29,39 +29,36 @@ sudo su -c 'echo "APT::Get::force-yes \"true\";" >> /etc/apt/apt.conf.d/pkgevalf
 # Uncomment following line to check it did indeed work
 #cat /etc/apt/apt.conf.d/pkgevalforceyes
 
+
 #######################################################################
-# Upgrade installation and install Julia and 
+# Upgrade the installation and install Julia 
 sudo apt-get update    # Pull in latest versions
 sudo apt-get upgrade   # Upgrade system packages
-# Use first argument to distinguish between the versions
+# Use first argument to script to distinguish between the versions
 if [ "$1" == "release" ]
 then
-    wget -O julia03.tar.gz https://julialang.s3.amazonaws.com/bin/linux/x64/0.3/julia-0.3-latest-linux-x86_64.tar.gz
-    mkdir julia03
-    tar -zxvf julia03.tar.gz -C ./julia03 --strip-components=1
-    export PATH="${PATH}:/home/vagrant/julia03/bin/"
-    # Retain PATH to make it easier to use VM for debugging
-    echo "export PATH=\"\${PATH}:/home/vagrant/julia03/bin/\"" >> /home/vagrant/.profile
+    wget -O julia.tar.gz https://julialang.s3.amazonaws.com/bin/linux/x64/0.3/julia-0.3-latest-linux-x86_64.tar.gz
 else
-    wget -O julia04.tar.gz https://status.julialang.org/download/linux-x86_64
-    mkdir julia04
-    tar -zxvf julia04.tar.gz -C ./julia04 --strip-components=1
-    export PATH="${PATH}:/home/vagrant/julia04/bin/"
-    # Retain PATH to make it easier to use VM for debugging
-    echo "export PATH=\"\${PATH}:/home/vagrant/julia04/bin/\"" >> /home/vagrant/.profile
+    wget -O julia.tar.gz https://status.julialang.org/download/linux-x86_64
 fi
+mkdir julia
+tar -zxvf julia.tar.gz -C ./julia --strip-components=1
+export PATH="${PATH}:/home/vagrant/julia/bin/"
+# Retain PATH to make it easier to use VM for debugging
+echo "export PATH=\"\${PATH}:/home/vagrant/julia/bin/\"" >> /home/vagrant/.profile
+
 
 #######################################################################
 # Install any dependencies that aren't handled by BinDeps
 # Somethings can't be or don't make sense to be installed by BinDeps,
 # so we will do them manually. Package maintainers can submit PRs to
 # extend this list as needed.
-# Need git of course, doesn't come by default
+# Need git of course, doesn't come by default in the image
 sudo apt-get install git
 # Need X virtual frame buffer for many packages
 sudo apt-get install xvfb
 # Need GMP for e.g. GLPK, why not get some PCRE too
-sudo apt-get install libpcre3-dev libgmp-dev 
+sudo apt-get install libpcre3-dev libgmp-dev
 # Need gfortran for e.g. GLMNet.jl, Ipopt.jl, KernSmooth.jl...
 sudo apt-get install gfortran pkg-config
 # Need unzip for e.g. Blink.jl, FLANN.jl
@@ -85,32 +82,50 @@ sudo apt-get install python-matplotlib
 sudo apt-get install python-pip
 sudo pip install xlrd
 
-#######################################################################
-# Install PackageEvaluator
-julia -e "Pkg.init(); Pkg.clone(\"https://github.com/IainNZ/PackageEvaluator.jl.git\")"
 
-# Run PackageEvaluator
-# We'll tee to dummy file to swallow any error
-# Make results folders
+#######################################################################
+# Install PackageEvaluator and prepare to run
+julia -e "Pkg.init(); Pkg.clone(\"https://github.com/IainNZ/PackageEvaluator.jl.git\")"
+# Make results folders. Folder name is second argument to this script.
+# These folders are shared - i.e. we are writing to outside the VM,
+# most likely the PackageEvaluator.jl/scripts folder.
 rm -rf /vagrant/$2
 mkdir /vagrant/$2
 cd /vagrant/$2
-# Make folder for where tested packages should go
-# Note that its important for it to not be in the /vagrant/
-# folder as that seems to mess with symlinks quite badly
-# See https://github.com/JuliaOpt/Ipopt.jl/issues/31
+# Make folder for where tested packages should go.
+# We could use the default, but then it gets messy because that is
+# where PackageEvaluator itself lives (and its dependencies).
+# Its important for it to not be in the /vagrant/ folder as hat seems
+# to mess with symlinks quite badly.
+# See https://github.com/JuliaOpt/Ipopt.jl/issues/31 for discussion
+# about this breaking a build.
 TESTPKG="/home/vagrant/testpkg"
 mkdir $TESTPKG
 # Initialize METADATA for testing
 JULIA_PKGDIR=$TESTPKG julia -e "Pkg.init(); println(Pkg.dir())"
+
+
+#######################################################################
+# Run PackageEvaluator
 if [ "$2" == "release" ]
 then
+    # For every package name...
     for f in /home/vagrant/.julia/v0.3/METADATA/*;
     do
-        pkgname=$(basename "$f")
-        JULIA_PKGDIR=$TESTPKG timeout 1800s julia -e "Pkg.add(\"${pkgname}\")" 2>&1 | tee PKGEVAL_${pkgname}_add.log
-        julia -e "using PackageEvaluator; eval_pkg(\"${pkgname}\",loadpkgadd=true,juliapkg=\"${TESTPKG}\",jsonpath=\"./\")" | tee catcherr
-        JULIA_PKGDIR=$TESTPKG julia -e "Pkg.rm(\"${pkgname}\")"
+        # Extract just the package name from the path
+        PKGNAME=$(basename "$f")
+        # Attempt to add the package. We give it half an hour - most
+        # use far less, some do push it. CoinOptServices.jl set the
+        # standard for build time and memory consumption, see
+        # https://github.com/IainNZ/PackageEvaluator.jl/issues/83
+        # The log for adding the package will go in the results folder.
+        JULIA_PKGDIR=$TESTPKG timeout 1800s julia -e "Pkg.add(\"${PKGNAME}\")" 2>&1 | tee PKGEVAL_${pkgname}_add.log
+        julia -e "using PackageEvaluator; eval_pkg(\"${PKGNAME}\",loadpkgadd=true,juliapkg=\"${TESTPKG}\",jsonpath=\"./\")" | tee catcherr
+        # Finish up by removing the package. Doesn't actually remove
+        # it in the sense of deleting the files - this helps the
+        # overall process run faster, if my understanding of how
+        # the .trash folder works is correct.
+        JULIA_PKGDIR=$TESTPKG julia -e "Pkg.rm(\"${PKGNAME}\")"
     done
 elif [ "$2" == "releaseAL" ]
 then
@@ -161,6 +176,7 @@ then
 fi
 
 
+#######################################################################
 # Bundle results together
 echo "Bundling results"
 cd /vagrant/
@@ -172,4 +188,5 @@ else
 fi
 
 
+#######################################################################
 echo "Finished normally! $1  $2"
