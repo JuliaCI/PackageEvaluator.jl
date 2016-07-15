4d8fb450d790fd08d3a1860a8a8bcd4690b333b8
ranjanan <benditlikeranjan@gmail.com>
2016-07-15 12:39:47 +0530

    Add requisite libraries for ArrayFire

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -89,6 +89,13 @@ export PYTHON=""
 sudo apt-get install wamerican
 # Need xmllint (and others?) for XMLDict.jl
 sudo apt-get install libxml2-utils
+# ArrayFire
+# ArrayFire
+sudo apt-get install libfreeimage-dev libatlas3gf-base libfftw3-dev cmake
+wget http://ci.arrayfire.org/userContent/Linux/ArrayFire-no-gl-v3.3.2_Linux_x86_64.sh
+sudo chmod +x ArrayFire-no-gl-v3.3.2_Linux_x86_64.sh
+sudo ./ArrayFire-no-gl-v3.3.2_Linux_x86_64.sh --exclude-subdir --prefix=/usr/local
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 
 
 #######################################################################
