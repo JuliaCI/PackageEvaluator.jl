c5231ef6c09c413b6e2e4a5fdcbfab7f510ee310
Tony Kelman <tony@kelman.net>
2016-08-02 19:08:24 -0400

    fix when quotes are not provided

--- scripts/summary.sh
+++ scripts/summary.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # needs jq installed (https://stedolan.github.io/jq/download)
 # usage: ./summary.sh 0.4*/*.json
-for i in $(ls $1); do
+for i in $@; do
   echo "$(basename $i) $(jq .status $i)"
 done
