eb1483ab7d155893797e89f5bfbe26d8ca5bc28d
Tony Kelman <tony@kelman.net>
2016-12-29 04:19:28 -0500

    revert kill-after flag
    seems to make things worse not better

--- src/preptest.jl
+++ src/preptest.jl
@@ -38,7 +38,7 @@ function prepare_test()
     if get(PKGOPTS, pkg_name, :NORMAL) == :XVFB
         print(fp, "xvfb-run ")
     end
-    print(fp, "$TIMEOUTPATH --kill-after=30s 2400s ")
+    print(fp, "$TIMEOUTPATH 2400s ")
     print(fp, "julia -e 'versioninfo(true); Pkg.test(\"", pkg_name, "\")'")
     print(fp, " 2>&1 | tee PKGEVAL_", pkg_name, "_test.log")
     close(fp)
